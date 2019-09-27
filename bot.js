e0e947672b7f557b4cd625fdd17db841db47dc90
anwar1938anwar <55034228+anwar1938anwar@users.noreply.github.com>
2019-09-27 18:22:41 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "#";
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`سعر البوت 15 الف اذا بدك تعال خاص ---> Anwar#2279`,"https://discordapp.com/channels/@me/626451542794567715")
+client.user.setStatus("dnd")
+});
+
+
+client.login(process.env.BOT_TOKEN);
